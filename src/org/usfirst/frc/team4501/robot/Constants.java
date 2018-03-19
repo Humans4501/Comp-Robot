a477553483f9e0ee3154819781ef26611e266d9b
HUMANS <nrsrobotics@gmail.com>
2018-03-19 14:21:42 -0700

    Added a Constants class for autonomous commands.

---
+++ src/org/usfirst/frc/team4501/robot/Constants.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team4501.robot;
+
+public class Constants {
+	public static final double AUTO_SEGMENT_1 = 0.3;
+	public static final double AUTO_SEGMENT_2 = 0.3;
+	public static final double AUTO_SEGMENT_3 = 0.3;
+
+	private Constants() {
+	}
+}
